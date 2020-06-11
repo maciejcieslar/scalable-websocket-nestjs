8950639208cf2d59c59f69669636d9b963d841f8
Mosly <moritz.wenko@gmx.net>
2020-06-11 14:21:48 +0200

    refactor: missing semicolons added in socket-state.service. npm audit fix for package.json to decrease number of vulnerabilities.

--- src/app/shared/socket-state/socket-state.service.ts
+++ src/app/shared/socket-state/socket-state.service.ts
@@ -1,47 +1,47 @@
-import { Injectable } from '@nestjs/common'
-import { Socket } from 'socket.io'
+import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
 
 @Injectable()
 export class SocketStateService {
-  private socketState = new Map<string, Socket[]>()
+  private socketState = new Map<string, Socket[]>();
 
   public remove(userId: string, socket: Socket): boolean {
-    const existingSockets = this.socketState.get(userId)
+    const existingSockets = this.socketState.get(userId);
 
     if (!existingSockets) {
-      return true
+      return true;
     }
 
-    const sockets = existingSockets.filter(s => s.id !== socket.id)
+    const sockets = existingSockets.filter(s => s.id !== socket.id);
 
     if (!sockets.length) {
-      this.socketState.delete(userId)
+      this.socketState.delete(userId);
     } else {
-      this.socketState.set(userId, sockets)
+      this.socketState.set(userId, sockets);
     }
 
-    return true
+    return true;
   }
 
   public add(userId: string, socket: Socket): boolean {
-    const existingSockets = this.socketState.get(userId) || []
+    const existingSockets = this.socketState.get(userId) || [];
 
-    const sockets = [...existingSockets, socket]
+    const sockets = [...existingSockets, socket];
 
-    this.socketState.set(userId, sockets)
+    this.socketState.set(userId, sockets);
 
-    return true
+    return true;
   }
 
   public get(userId: string): Socket[] {
-    return this.socketState.get(userId) || []
+    return this.socketState.get(userId) || [];
   }
 
   public getAll(): Socket[] {
-    const all = []
+    const all = [];
 
-    this.socketState.forEach(sockets => all.push(sockets))
+    this.socketState.forEach(sockets => all.push(sockets));
 
-    return all
+    return all;
   }
 }
